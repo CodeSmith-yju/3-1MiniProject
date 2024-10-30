db8069c1c6a33fda383df7da031a3aa965c53385
SGB1801082 <145640640+SGB1801082@users.noreply.github.com>
2024-10-30 21:44:51 +0900

    update
    갈피를 못 잡겠네

---
+++ Assets/Scripts/Scene1/PartyDetails/PartyDetails.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyDetails : MonoBehaviour
+{
+
+
+    public void Test()
+    {
+        //gmaeObject.SetActive(false);
+        Debug.Log("외부영역 클릭");
+    }
+}
