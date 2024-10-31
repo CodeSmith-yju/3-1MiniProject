b10b788b48d213ad6bf009299eda53d31f6cd875
SGB1801082 <145640640+SGB1801082@users.noreply.github.com>
2024-10-31 12:02:46 +0900

    fix
    afsasdfafsd

--- Assets/Scripts/Scene1/PartyDetails/PartyDetails.cs
+++ Assets/Scripts/Scene1/PartyDetails/PartyDetails.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PartyDetails : MonoBehaviour
 {
+    /*public List<GameObject> Btns = new();
+    public Transform tf_Content;
+    public TextMeshProUGUI text;*/
 
+
+    public void Init(PartyData _pd)//GuiMgr의 List<PartySlot>인 lastDefartual의 partyData를통해 목록을만들것.
+    {
+        //if (Btns.Count < 1)
+    }
+
+    public void OpenCharactorDetails(PartyData _pd)
+    {
+
+    }
 
     public void Test()
     {
