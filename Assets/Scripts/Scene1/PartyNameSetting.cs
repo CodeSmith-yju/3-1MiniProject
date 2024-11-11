452fbb520d9d4a2b70d1e9c84b0c93309b6df591
SGB1801082 <145640640+SGB1801082@users.noreply.github.com>
2024-11-11 12:36:12 +0900

    fix
    프리스트 이름 새로고침 관련 코드 수정.
    
    복합할당과 유니티 인스펙터시스템의
    이해도가 상승함...알고싶지않았어

--- Assets/Scripts/Scene1/PartyNameSetting.cs
+++ Assets/Scripts/Scene1/PartyNameSetting.cs
@@ -10,7 +10,7 @@ public class PartyNameSetting : MonoBehaviour
     public List<string> mageNames;
     public List<string> priestNames;
     public List<string> demonNames;
-
+    
 
     /* //한국어이름도 일단 만들어뒀음
         knightNames = new List<string> { "알프레드", "에드먼드", "해럴드", "제프리", "월터", "가이", "레이먼드", "오스윈", "아이보", "휴" };
@@ -39,33 +39,35 @@ public class PartyNameSetting : MonoBehaviour
     }
     public void RefreshiNameList()
     {
-        knightNames ??= new List<string> { "Alaric", "Gawain", "Roland", "Lancelot", "Arthas", "Tristan", "Bedivere", "Galahad", "Percival", "Kay" };
-        archerNames ??= new List<string> { "Robin", "Elduin", "Faolan", "Nimue", "Ayla", "Vara", "Rylai", "Orin", "Shara", "Elandra" };
-        mageNames ??= new List<string> { "Merlin", "Gandalf", "Morgana", "Zatanna", "Aegis", "Althas", "Luna", "Arcanis", "Solara", "Faye" };
-        priestNames ??= new List<string> { "Aldric", "Benedict", "Elspeth", "Ishara", "Tobias", "Lucius", "Cyril", "Anselm", "Seraphine", "Eldora" };
-        demonNames ??= new List<string> { "Garruk", "Thorne", "Drex", "Magnus", "Borin", "Huldan", "Grimnir", "Varg", "Ragnar", "Krogan" };
-
-        //knightNames ??= new List<string> { "알프레드", "에드먼드", "해럴드", "제프리", "월터", "가이", "레이먼드", "오스윈", "아이보", "휴" };
-        //archerNames ??= new List<string> { "에일린", "길다", "이소벨", "엘레노어", "콜린", "레이놀드", "아델라", "에디스", "오스카", "앨런" };
-        //mageNames ??= new List<string> { "마이클", "리처드", "에드워드", "가브리엘", "토머스", "알렉산더", "제이콥", "크리스토퍼", "필립", "도미닉", };
-
+        /// <summary>
+        /// public변수로 선언하면 유니티 인스펙터창에보이는 SerialLize시스템때문에 자동으로 null이 아니라 비어있는 배열로 초기화되어서 그런다고함
+        /// nameList ??= new List<string> { "A", "B", "C" }; 이런거 못쓴다는 뜻. 
+        /// 비어있는 리스트로 초기화되었기때문에 null검사 하는 복합할당 검사문(" ??= ")안먹히고, Count가 0인배열이라 Count검사해야됨.
+        /// </summary>
         if (knightNames.Count < 10)
         {
             knightNames.Clear();
             knightNames = new List<string> { "Alaric", "Gawain", "Roland", "Lancelot", "Arthas", "Tristan", "Bedivere", "Galahad", "Percival", "Kay" };
-            //knightNames = new List<string> { "알프레드", "에드먼드", "해럴드", "제프리", "월터", "가이", "레이먼드", "오스윈", "아이보", "휴" };
         }
         if (archerNames.Count < 10)
         {
             archerNames.Clear();
             archerNames = new List<string> { "Robin", "Elduin", "Faolan", "Nimue", "Ayla", "Vara", "Rylai", "Orin", "Shara", "Elandra" };
-            //archerNames = new List<string> { "에일린", "길다", "이소벨", "엘레노어", "콜린", "레이놀드", "아델라", "에디스", "오스카", "앨런" };
         }
         if (mageNames.Count < 10)
         {
             mageNames.Clear();
             mageNames = new List<string> { "Merlin", "Gandalf", "Morgana", "Zatanna", "Aegis", "Althas", "Luna", "Arcanis", "Solara", "Faye" };
-            //mageNames = new List<string> { "마이클", "리처드", "에드워드", "가브리엘", "토머스", "알렉산더", "제이콥", "크리스토퍼", "필립", "도미닉", };
+        }
+        if (priestNames.Count < 10)
+        {
+            priestNames.Clear();
+            priestNames = new List<string> { "Aldric", "Benedict", "Elspeth", "Ishara", "Tobias", "Lucius", "Cyril", "Anselm", "Seraphine", "Eldora" };
+        }
+        if (demonNames.Count < 10)
+        {
+            demonNames.Clear();
+            demonNames = new List<string> { "Garruk", "Thorne", "Drex", "Magnus", "Borin", "Huldan", "Grimnir", "Varg", "Ragnar", "Krogan" };
         }
     }
 }
